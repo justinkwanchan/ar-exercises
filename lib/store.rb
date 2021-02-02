4af0105ee3e47aec6e2db7ff51295066ab689c5c
justinkwanchan <justinkwanchan@gmail.com>
2021-02-02 04:08:49 +0000

    Finish BONUS in exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,12 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :at_least_one_of_mens_or_womens
+
+  def at_least_one_of_mens_or_womens
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:mens_apparel, "must carry at least one of the men's or women's apparel")
+    end
+  end
+  
 end
